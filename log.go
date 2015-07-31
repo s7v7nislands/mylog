6d33bc0132f45f964d325c8a8dbba019f5fd59d7
s7v7nislands <s7v7nislands@gmail.com>
2015-07-31 11:49:10 +0800

    1. 去掉logger group的支持 2. 增加缓存logger的支持

--- log.go
+++ log.go
@@ -2,6 +2,7 @@ package mylog
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strings"
@@ -13,6 +14,7 @@ const (
 	INFO
 	WARN
 	ERROR
+	FATAL
 )
 
 // debug的级别,字符串表示
@@ -21,6 +23,7 @@ var Levels = map[string]int{
 	"INFO":  INFO,
 	"WARN":  WARN,
 	"ERROR": ERROR,
+	"FATAL": FATAL,
 }
 
 // GetLevel返回日志级别,没有就返回DEBUG
@@ -35,177 +38,100 @@ func GetLevel(level string) int {
 // Logger表示有日志级别的
 type logger struct {
 	level int
+	l     io.Writer
 	*log.Logger
 }
 
-// loggerGroup表示可以输出到多个日志
-type LoggerGroup struct {
-	g []*logger
-}
-
-var stdLog = newLogger(INFO, log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile))
-var stdLogGroup = New(stdLog)
+var stdLog = New(INFO, os.Stderr, log.LstdFlags|log.Lshortfile)
 
 // New返回*Logger
-func newLogger(level int, log *log.Logger) *logger {
-	return &logger{level, log}
+func New(level int, l io.Writer, flag int) *logger {
+	return &logger{level: level, l: l, Logger: log.New(l, "", flag)}
 }
 
-// NewGroup返回*LoggerGroup
-func New(l ...*logger) *LoggerGroup {
-	g := &LoggerGroup{}
-	g.g = append(g.g, l...)
-
-	return g
-}
-
-func Init(level int, log *log.Logger) {
-	stdLogGroup.Init(level, log)
-}
-
-func AddHandler(level int, log *log.Logger) {
-	l := newLogger(level, log)
-	stdLogGroup.g = append(stdLogGroup.g, l)
-}
-
-func (g *LoggerGroup) Init(level int, log *log.Logger) {
-	l := newLogger(level, log)
-	g.g = []*logger{l}
-}
-
-func (g *LoggerGroup) AddHandler(level int, log *log.Logger) {
-	l := newLogger(level, log)
-	g.g = append(g.g, l)
-}
-
-func (l *logger) log(level int, format string, v ...interface{}) {
+func (l *logger) Log(level int, format string, v ...interface{}) {
 	if level < l.level {
 		return
 	}
 	l.Output(2, fmt.Sprintf(format, v...))
 }
 
-func (l *logger) debug(format string, v ...interface{}) {
+func (l *logger) Debug(format string, v ...interface{}) {
 	if l.level > DEBUG {
 		return
 	}
 	l.Output(2, fmt.Sprintf(format, v...))
 }
 
-func (l *logger) info(format string, v ...interface{}) {
+func (l *logger) Info(format string, v ...interface{}) {
 	if l.level > INFO {
 		return
 	}
 	l.Output(2, fmt.Sprintf(format, v...))
 }
 
-func (l *logger) warn(format string, v ...interface{}) {
+func (l *logger) Warn(format string, v ...interface{}) {
 	if l.level > WARN {
 		return
 	}
 	l.Output(2, fmt.Sprintf(format, v...))
 }
 
-func (l *logger) err(format string, v ...interface{}) {
+func (l *logger) Error(format string, v ...interface{}) {
 	if l.level > ERROR {
 		return
 	}
 	l.Output(2, fmt.Sprintf(format, v...))
 }
 
-func (g *LoggerGroup) Log(level int, format string, v ...interface{}) {
-	for _, l := range g.g {
-		if level < l.level {
-			continue
-		}
-		l.Output(2, fmt.Sprintf(format, v...))
-	}
+func (l *logger) GetOutput() io.Writer {
+	return l.l
 }
 
-func (g *LoggerGroup) Debug(format string, v ...interface{}) {
-	for _, l := range g.g {
-		if l.level > DEBUG {
-			continue
-		}
-		l.Output(2, fmt.Sprintf(format, v...))
-	}
+func (l *logger) Write(format string, v ...interface{}) {
+	l.Output(2, fmt.Sprintf(format, v...))
 }
 
-func (g *LoggerGroup) Info(format string, v ...interface{}) {
-	for _, l := range g.g {
-		if l.level > INFO {
-			continue
-		}
-		l.Output(2, fmt.Sprintf(format, v...))
-	}
-}
-
-func (g *LoggerGroup) Warn(format string, v ...interface{}) {
-	for _, l := range g.g {
-		if l.level > WARN {
-			continue
-		}
-		l.Output(2, fmt.Sprintf(format, v...))
-	}
-}
-
-func (g *LoggerGroup) Err(format string, v ...interface{}) {
-	for _, l := range g.g {
-		if l.level > ERROR {
-			continue
-		}
-		l.Output(2, fmt.Sprintf(format, v...))
-	}
+func Write(format string, v ...interface{}) {
+	stdLog.Output(2, fmt.Sprintf(format, v...))
 }
 
 func Log(level int, format string, v ...interface{}) {
-	for _, l := range stdLogGroup.g {
-		if level < l.level {
-			continue
-		}
-		l.Output(2, fmt.Sprintf(format, v...))
+	if level < stdLog.level {
+		return
 	}
+	stdLog.Output(2, fmt.Sprintf(format, v...))
 }
 
 func Fatal(format string, v ...interface{}) {
-	for _, l := range stdLogGroup.g {
-		l.Output(2, fmt.Sprintf("Fatal: "+format, v...))
-	}
+	stdLog.Output(2, fmt.Sprintf("Fatal: "+format, v...))
 	os.Exit(1)
 }
 
 func Debug(format string, v ...interface{}) {
-	for _, l := range stdLogGroup.g {
-		if l.level > DEBUG {
-			continue
-		}
-		l.Output(2, fmt.Sprintf("Debug: "+format, v...))
+	if stdLog.level > DEBUG {
+		return
 	}
+	stdLog.Output(2, fmt.Sprintf("Debug: "+format, v...))
 }
 
 func Info(format string, v ...interface{}) {
-	for _, l := range stdLogGroup.g {
-		if l.level > INFO {
-			continue
-		}
-		l.Output(2, fmt.Sprintf("Info: "+format, v...))
+	if stdLog.level > INFO {
+		return
 	}
+	stdLog.Output(2, fmt.Sprintf("Info: "+format, v...))
 }
 
 func Warn(format string, v ...interface{}) {
-	for _, l := range stdLogGroup.g {
-		if l.level > WARN {
-			continue
-		}
-		l.Output(2, fmt.Sprintf("Warn: "+format, v...))
+	if stdLog.level > WARN {
+		return
 	}
+	stdLog.Output(2, fmt.Sprintf("Warn: "+format, v...))
 }
 
-func Err(format string, v ...interface{}) {
-	for _, l := range stdLogGroup.g {
-		if l.level > ERROR {
-			continue
-		}
-		l.Output(2, fmt.Sprintf("Error: "+format, v...))
+func Error(format string, v ...interface{}) {
+	if stdLog.level > ERROR {
+		return
 	}
+	stdLog.Output(2, fmt.Sprintf("Error: "+format, v...))
 }
